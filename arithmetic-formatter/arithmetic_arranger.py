1745a8f46345ebc49c3ac312cbf7c2333598aff4
Mark <deafiant+github@gmail.com>
2022-01-08 12:18:30 +1100

    Passed first test! - input list length check

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,12 @@
-def arithmetic_arranger(problems):
-
-
-    return arranged_problems
+def arithmetic_arranger(problem_list, show_answers=False):
+    """Formats a list of arithmetic problems arranged vertically and side-by-side.
+    
+    """
+    
+    num_of_problems = len(problem_list)
+    if num_of_problems > 5:
+        return "Error: Too many problems."
+    
+    
+if __name__ == '__main__':
+    print("I am the main man.")
