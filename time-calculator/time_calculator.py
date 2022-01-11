b985bf8c0d51bfb16fe998ec90fcae8d1404f14b
Mark G <deafiant+github@gmail.com>
2022-01-11 11:23:19 +1100

    Added day of week output: Function passes all tests

--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -29,15 +29,24 @@ def add_time(start, duration, day=None):
         extra = " (next day)"
     elif add_days > 1:
         extra = f" ({add_days} days later)"
+    days_of_week = ["monday", "tuesday", "wednesday",
+                    "thursday", "friday", "saturday", "sunday"]
+    if day:
+        if day.lower() in days_of_week:
+            day_index = days_of_week.index(day.lower())
+            day_index += add_days
+            new_day = days_of_week[day_index % 7]
+            extra = f", {new_day.capitalize()}" + extra
+        else:
+            return f"Error: '{day}' is not a day of the week."
 
 
-    new_time = f"{new_hour}:{new_min:02} {new_ampm}{extra}"
-
-    return new_time
+    return f"{new_hour}:{new_min:02} {new_ampm}{extra}"
 
 if __name__ == "__main__":
-    print("1:38 PM,  ", add_time("11:26 AM", "2:12"))
-    print("12:30 PM,  ", add_time("11:30 AM", "1:00"))
-    print("12:15 AM,  ", add_time("11:15 PM", "1:00"))
-    print(add_time("8:26 AM", "55:09"))
-    print(add_time("10:35 pm", "14:40", "Sunday"))
+    print("11:26 AM + 2:12 is ", add_time("11:26 AM", "2:12"))
+    print("11:30 AM + 1:00 is ", add_time("11:30 AM", "1:00"))
+    print("11:15 PM + 1:40 is ", add_time("11:15 PM", "1:40"))
+    print("8:26 AM + 55:09 is ", add_time("8:26 AM", "55:09"))
+    print("10:35 pm Sunday + 14:40 is ", add_time("10:35 pm", "14:40", "Sunday"))
+    print("10:35 pm boo + 14:40 is ", add_time("10:35 pm", "14:40", "boo"))
