db97c7afaeb703cf0664007699107bf5a1baa735
Mark G <deafiant+github@gmail.com>
2022-01-16 19:03:19 +1100

    WIP: most methods of class finished

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,7 +1,34 @@
 class Category:
-
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+        self.balance = 0
+    def deposit(self, amount, description=""):
+        transaction = {}
+        transaction["amount"], transaction["description"] = amount, description
+        self.ledger.append(transaction)
+        self.balance += amount
+    def get_balance(self):
+        return self.balance
+    def check_funds(self, amount):
+        return self.balance > amount
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            transaction = {}
+            transaction["amount"], transaction["description"] = -amount, description
+            self.ledger.append(transaction)
+            self.balance -= amount
+            return True
+        else:
+            return False
+    def transfer(self, amount, catergory):
+        pass
 
 
 
 
 def create_spend_chart(categories):
+    pass
+
+if __name__ == "__main__":
+    pass
