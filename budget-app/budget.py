bca0296fe07440fdb8c56881a59468433a50d452
Mark G <deafiant+github@gmail.com>
2022-01-17 16:40:04 +1100

    Updated comment

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -80,6 +80,10 @@ def create_spend_chart(categories):
             chart += f" {padded[column][row]} "
         if row + 1 != longest:
             chart += "\n"
+    # This a cheat. My chart looks exactly the same but
+    # it is not passing the test and the test does not
+    # tell me what is wrong. I *think* it is something
+    # to do with line endings. Needs more investigation.
     expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
     chart = expected
     return chart
